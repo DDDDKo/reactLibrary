93ea3241fbf7b69ed54653fc53a107011ca647ab
shimtaewook <to1583@naver.com>
2024-04-18 11:52:59 +0900

    2024-04-18 11:50

--- src/axios/AxiosLibrary.tsx
+++ src/axios/AxiosLibrary.tsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React from "react";
 
 // axios 패키지(라이브러리) :
@@ -8,5 +9,38 @@ import React from "react";
 
 // - npm i axios
 export default function AxiosLibrary() {
+
+    const GET_URL = 'http://localhost:4100/api/v1/user/asd@email.com';
+
+    // axios 객체
+    // - axios 객체는 http method에 해당하는 함수들을 포함하고 있음
+    // - 각각의 http method 함수는 URL, option을 매개변수로 받음
+    // - 각각의 http method 함수는 결과를 promise 형태로 반환
+    axios.get(GET_URL).
+    // then => 각각의 http method 함수는 결과를 then으로 받아을땐 response객체를 매개변수로 받는 콜백함수를 전달해야함
+    then(response => {
+        // response 객체는 status, header, data(body)를 포함하고있음
+        // response.data 와 같이 일정 부분만 가져올 수 있음
+        // then 작업은 status code가 400~500 번대가 아닌 response를 받아옴
+        console.log(response.data);
+    }).
+    catch(error => {
+        // 해당 request에 대해서 request가 실패하거나 response의 status code가 400~500번대이면 catch로 AxiosError 객체를 받아옴
+        // response가 존재하는 에러는 error객체에 response가 포함되어 있음
+        console.log(error);
+    });
+
+    // axios의 option 매개변수
+    // - request의 각종 설정을 조작할 때 사용됨 (보통 header를 조작할때 사용)
+    axios.get(GET_URL, {
+        headers : { 'Authorization' : 'Bearer 4567894' },
+        params : {'time' : '1144'} 
+    })
+
+    // axios의 request body 매개변수
+    // - post, patch, put 함수는 두번째 매개변수로 request body 데이터를 전달함
+    const PATCH_URL = 'http://localhost:4100/api/v1/user/nickname';
+    axios.patch(PATCH_URL, { email : 'asd@email.com', nickname : '코스피' });
+
     return <div>AxiosLibrary</div>;
 }
