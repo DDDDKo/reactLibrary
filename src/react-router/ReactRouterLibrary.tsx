eaa92ebe09f88cc6e8dbf00b5090bda2f204b6e7
shimtaewook <to1583@naver.com>
2024-04-18 10:49:40 +0900

    2024-04-18 10:50

--- src/react-router/ReactRouterLibrary.tsx
+++ src/react-router/ReactRouterLibrary.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { time } from "console";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams, useSearchParams } from "react-router-dom";
 
 // react-router 패키지(라이브러리) :
 // - react의 SPA(Single Page Application)에서 라우팅을 구현하기 위한 라이브러리
@@ -71,9 +72,19 @@ export default function ReactRouterLibrary() {
 // http://localhost:3000/page1
 // http://localhost:3000/router/page1
 export function ReactRouterPage1 () {
+
+    // useParams :
+    // - <Route> 컴포넌트의 path 속성에 매칭되는 동적 URL 패턴에 따른 데이터를 받는 훅 함수
+    // - 해당 URL 경로에 대한 모든 동적 URL 패턴을 name, vlaue 형태로 받아옴
+    const {value} = useParams();
+
+    useEffect( () => {
+        console.log(value);
+    } )
+
     return (
         <div>
-            <h1>페이지 1</h1>
+            <h1>페이지 1 : {value} </h1>
             {/* <a href="http://localhost:3000/router">홈으로!</a> */}
             <Link to={'/router'}>홈으로!</Link>
         </div>
@@ -83,6 +94,22 @@ export function ReactRouterPage1 () {
 // http://localhost:3000/page2
 // http://localhost:3000/router/page2
 export function ReactRouterPage2 () {
+
+    // useSearchParams :
+    // - 현재 URL에 있는 쿼리 문자열을 읽을 때 사용하는 함수
+    // - 쿼리 문자열에 대한 배열을 반환
+
+    // 쿼리 문자열 => url뒤에 ?뒤에 오는 값들 보통 key=value로 이루어져 있음 
+    // useSearchParams를 사용하면 ?뒤에 오는 값만 읽음
+    // path를 읽을때는 ?뒤에 오는 쿼리 문자열은 읽지 않음
+
+    const [queryString] = useSearchParams();
+    const time = queryString.get('time');
+
+    useEffect( () => {
+        console.log(time);
+    }, [])
+
     return (
         <div>
             <h1>페이지 2</h1>
